c1dcfee374aced5fa90221028f62ac83ac861536
Gábor Szijártó <gabor@snowrake.ai>
2025-05-30 08:37:05 +0200

    [ref] removal of unused 3rd party dependency
    No benchmarks and test-cases setup then no need for the deps.

--- cmake/libs/benchmark.cmake
+++
@@ -1,9 +0,0 @@
-FetchContent_Declare(
-	benchmark
-	GIT_REPOSITORY https://github.com/google/benchmark
-	GIT_TAG		   v1.9.3
-	GIT_SHALLOW TRUE
-	GIT_PROGRESS TRUE
-)
-
-option(BENCHMARK_ENABLE_TESTING "Enable testing of the benchmark library." OFF)
